a1b2e642e325fcb90ee2783b4d524d4df6be8e80
Jeffry Luqman <jeffry.luqman@gmail.com>
2022-02-20 17:04:25 +0700

    add fmt

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ grest version
 grest new app        = Create a new grest app in the current folder
 grest new service    = Create a new service of grest app in the current folder
 grest version        = Check grest-cli version
+grest fmt            = Formatting struct tag
 ```
 
 ## License
--- help.go
+++ help.go
@@ -8,5 +8,7 @@ func UnknownCommand() {
 	fmt.Println(`Unknown command, usage :
     grest new app        = Create a new grest app in the current folder
     grest new service    = Create a new service of grest app in the current folder
-    grest version        = Check grest-cli version`)
+    grest version        = Check grest-cli version
+    grest fmt            = Formatting struct tag
+    `)
 }
